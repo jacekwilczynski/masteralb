99b20a6f4523636df2dbd0b84552c95534bd909f
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-08 16:12:31 +0200

    Set global font

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Page from 'pages/FormsShowcase/index';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from 'registerServiceWorker';
+import 'style.css';
 
 ReactDOM.render(<Page />, document.getElementById('root'));
 registerServiceWorker();
