0fbbbf1f02c9d7980a9514f3757b2603f21a1221
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-08 14:27:20 +0200

    Initial

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(null, document.getElementById('root'));
+registerServiceWorker();
