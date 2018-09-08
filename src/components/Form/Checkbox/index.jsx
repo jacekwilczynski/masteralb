a40624ea340a62e2f68a46448e7788607ff12dab
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-08 17:55:04 +0200

    Add checkboxes

---
+++ src/components/Form/Checkbox/index.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import './style.css';
+
+const Checkbox = ({ label }) => (
+  <label className="checkbox">
+    <span className="checkbox__container">
+      <input className="checkbox__input" type="checkbox" />
+      <span className="checkbox__box">
+        <span className="checkbox__mark" />
+      </span>
+    </span>
+    <span className="checkbox__label">{label}</span>
+  </label>
+);
+
+Checkbox.propTypes = {
+  label: PropTypes.string.isRequired
+};
+
+export default Checkbox;
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,18 +1,21 @@
+import Checkbox from 'components/Form/Checkbox';
 import TextField from 'components/Form/TextField';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Form = ({ textFields }) => (
+const Form = ({ textFields, checkbox }) => (
   <form action="#" className="form">
     {textFields.map(textField => (
       <TextField key={textField.name} {...textField} />
     ))}
+    {checkbox && <Checkbox label={checkbox} />}
   </form>
 );
 
 Form.propTypes = {
   textFields: PropTypes.arrayOf(PropTypes.shape(TextField.PropTypes).isRequired)
-    .isRequired
+    .isRequired,
+  checkbox: PropTypes.string
 };
 
 export default Form;
