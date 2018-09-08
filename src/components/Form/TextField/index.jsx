f53834b36f7d101ce1c3b7f05f3759c9ad0787f0
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-08 17:29:10 +0200

    Do not render non-existent icons

--- src/components/Form/TextField/index.jsx
+++ src/components/Form/TextField/index.jsx
@@ -3,19 +3,24 @@ import React from 'react';
 import icons from './icons';
 import './style.css';
 
-const TextField = ({ type, name, label }) => (
-  <label className={'text-field text-field--' + type} aria-label={label}>
-    <span className="text-field__icon-container">
-      <img src={icons[type]} alt="" className="text-field__icon" />
-    </span>
-    <input
-      type="text"
-      name={name}
-      placeholder={label}
-      className="text-field__input"
-    />
-  </label>
-);
+const TextField = ({ type, name, label }) => {
+  const icon = icons[type];
+  return (
+    <label className={'text-field text-field--' + type} aria-label={label}>
+      {icon && (
+        <span className="text-field__icon-container">
+          <img src={icon} alt="" className="text-field__icon" />
+        </span>
+      )}
+      <input
+        type="text"
+        name={name}
+        placeholder={label}
+        className="text-field__input"
+      />
+    </label>
+  );
+};
 
 TextField.propTypes = {
   type: PropTypes.string.isRequired,
