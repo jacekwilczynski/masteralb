c04afd265a467febd171a6ebb656a58e1b5d1a41
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-08 16:44:26 +0200

    Load forms from yaml files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import formUrls from 'forms';
 import Page from 'pages/FormsShowcase/index';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from 'registerServiceWorker';
 import 'style.css';
 import { loadMany } from 'utils/loadYaml';
-import formUrls from 'forms';
 
 loadMany(formUrls).then(forms => {
   ReactDOM.render(<Page forms={forms} />, document.getElementById('root'));
