c1775d6e9baaa11108cbb23307aa8b6c763055a2
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-09 19:20:29 +0200

    Use main tag for container

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './style.css';
 
 const Container = ({ children }) => (
-  <div className="container">
+  <main className="container">
     {React.Children.map(children, child => (
       <div className="container__element">{child}</div>
     ))}
-  </div>
+  </main>
 );
 
 export default Container;
