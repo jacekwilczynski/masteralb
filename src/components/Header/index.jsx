8335c2d1693c77d2d76ddbcf953c5dbece1adf09
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-09 16:41:03 +0200

    Rename component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import logo from './logo.png';
 import './style.css';
 
-const TopLogo = () => (
+const Header = () => (
   <header className="header">
     <img src={logo} alt="Masteralb logo" className="header__logo"/>
   </header>
 );
 
-export default TopLogo;
+export default Header;
