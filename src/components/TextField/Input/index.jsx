8e0e90efb936f8cda7c8eb3287ed34917a5365fd
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-09 16:38:21 +0200

    Extract text field input component

---
+++ src/components/TextField/Input/index.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import './style.css';
+
+const Input = ({ name, label, type, dropdown }) => {
+  const InputTag = type === 'multiline' ? 'textarea' : 'input';
+  return (
+    <InputTag
+      type="text"
+      name={name}
+      placeholder={label}
+      aria-label={label}
+      className={
+        'text-field__input' +
+        (type === 'multiline' ? ' text-field--multiline' : '') +
+        (dropdown ? ' text-field__input--dropdown' : '')
+      }
+    />
+  );
+};
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  dropdown: PropTypes.bool
+};
+
+export default Input;
--- src/components/TextField/index.jsx
+++ src/components/TextField/index.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import icons from './icons/index';
+import Input from './Input';
 import './style.css';
 
-const TextField = ({ type, name, label, dropdown }) => {
+const TextField = props => {
+  const { type, dropdown } = props;
   const icon = icons[type];
-  const InputTag = type === 'multiline' ? 'textarea' : 'input';
   return (
     <label className={'text-field text-field--' + type}>
       {icon && (
@@ -13,17 +14,7 @@ const TextField = ({ type, name, label, dropdown }) => {
           <img src={icon} alt="" className="text-field__icon" />
         </span>
       )}
-      <InputTag
-        type="text"
-        name={name}
-        placeholder={label}
-        aria-label={label}
-        className={
-          'text-field__input' +
-          (type === 'multiline' ? ' text-field--multiline' : '') +
-          (dropdown ? ' text-field__input--dropdown' : '')
-        }
-      />
+      <Input {...props} />
       {dropdown && <span className="text-field__caret" />}
     </label>
   );
