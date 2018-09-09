29238a6f6454e362d3f8eb2666cbcc6b21587050
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-09 08:45:40 +0200

    Refactor

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -3,20 +3,35 @@ import React from 'react';
 import FormElement from './FormElement';
 import './style.css';
 
-const Form = ({ inputs }) => (
-  <form action="#" className="form">
-    {inputs.filter(input => input.type === 'text-field').map(input => (
-      <React.Fragment>
-        <FormElement key={input.name} {...input} />
-      </React.Fragment>
-    ))}
-    <div className="form__row">
-      {inputs.filter(input => input.type !== 'text-field').map(input => (
-        <FormElement key={input.name} {...input} />
+const split = condition => array => {
+  const matches = [];
+  const rest = [];
+  for (let item of array) {
+    const destination = condition(item) ? matches : rest;
+    destination.push(item);
+  }
+  return [matches, rest];
+};
+
+const Form = ({ inputs }) => {
+  const [textFields, others] = split(input => input.type === 'text-field')(
+    inputs
+  );
+  return (
+    <form action="#" className="form">
+      {textFields.map(input => (
+        <React.Fragment>
+          <FormElement key={input.name} {...input} />
+        </React.Fragment>
       ))}
-    </div>
-  </form>
-);
+      <div className="form__row">
+        {others.map(input => (
+          <FormElement key={input.name} {...input} />
+        ))}
+      </div>
+    </form>
+  );
+};
 
 Form.propTypes = {
   inputs: PropTypes.arrayOf(FormElement.propTypes).isRequired
