70c8435e1ddf66880a49629e7af9e1a960249e51
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-11 12:35:49 +0200

    Log rendered static html to the console

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import formUrls from 'forms';
 import Page from 'pages/FormsShowcase/index';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { renderToStaticMarkup } from 'react-dom/server';
 import registerServiceWorker from 'registerServiceWorker';
 import 'style.css';
 import { loadMany } from 'utils/loadYaml';
 
 loadMany(formUrls).then(forms => {
-  ReactDOM.render(<Page forms={forms} />, document.getElementById('root'));
+  const element = <Page forms={forms} />;
+  ReactDOM.render(element, document.getElementById('root'));
+  console.log(
+    renderToStaticMarkup(element)
+      .replace(/data:image[^"]*"/g, '"')
+      .replace(/>/g, '>\n')
+  );
 });
 
 registerServiceWorker();
