ee3f1f2c54686ed75f750cb28968d3d4d6d89b33
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-11 22:53:52 +0200

    Remove src attributes referring to files in the "static" directory

--- src/getHtml.js
+++ src/getHtml.js
@@ -1,11 +1,13 @@
 import { renderToStaticMarkup } from 'react-dom/server';
 
 const inlinedImageData = /src="data:image[^"]*"\s?/g;
+const localSrcs = /src="\/static[^"]*"\s?/g;
 const dataSrcAttributes = /data-src="([^"]*)"/g;
 
 const getHtml = element =>
   renderToStaticMarkup(element)
     .replace(inlinedImageData, '')
+    .replace(localSrcs, '')
     .replace(dataSrcAttributes, 'src="images/$1"')
     .replace(/\/>/g, '>')
     .replace(/>/g, '>\n')
