a63c64727938472d79f438da18b9e42af8eb0622
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-11 14:26:34 +0200

    Add gulp task to create index.css

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const beautifyHtml = require('gulp-html-beautify');
 const flatten = require('gulp-flatten');
 const path = require('path');
+const fileNames = require('gulp-filenames');
+const fs = require('fs');
 
 gulp.task('beautifyHtml', function() {
   return gulp
@@ -14,7 +16,20 @@ gulp.task('copyCss', function() {
   return gulp
     .src('src/**/*.css')
     .pipe(flatten())
+    .pipe(fileNames('styles'))
     .pipe(gulp.dest('build/styles'));
 });
 
-gulp.task('default', ['beautifyHtml', 'copyCss']);
+gulp.task('createIndexCss', ['copyCss'], function() {
+  const cssFiles = fileNames.get('styles');
+  fs.writeFileSync(
+    path.resolve(__dirname, 'build/styles/index.css'),
+    cssFiles
+      .filter(fileName => fileName.indexOf('--') < 0)
+      .map(fileName => `@import '${fileName}';\n`)
+      .join(''),
+    'utf-8'
+  );
+});
+
+gulp.task('default', ['beautifyHtml', 'createIndexCss']);
