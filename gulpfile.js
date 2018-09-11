ed2a4eaa8568d65510041d1725ca65152f777869
Jacek Wilczyński <jacekwilczynski5@gmail.com>
2018-09-11 14:07:37 +0200

    Add gulp task to copy CSS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 const gulp = require('gulp');
 const beautifyHtml = require('gulp-html-beautify');
+const flatten = require('gulp-flatten');
 const path = require('path');
 
-gulp.task('default', function() {
+gulp.task('beautifyHtml', function() {
   return gulp
     .src(path.resolve(__dirname, 'build/index.html'))
     .pipe(beautifyHtml({ indent_size: 2 }))
     .pipe(gulp.dest(path.resolve(__dirname, 'build/')));
 });
+
+gulp.task('copyCss', function() {
+  return gulp
+    .src('src/**/*.css')
+    .pipe(flatten())
+    .pipe(gulp.dest('build/styles'));
+});
+
+gulp.task('default', ['beautifyHtml', 'copyCss']);
